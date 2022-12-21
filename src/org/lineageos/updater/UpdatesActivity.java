886a7f1382a312bc1b8bef7b9eacde2cc8f3f0b3
JoshuaDoes <joshuadoespersonalstuff@gmail.com>
2022-12-21 02:24:11 -0500

    Updater: Drop ABperf and improve build list UI

--- src/org/lineageos/updater/UpdatesActivity.java
+++ src/org/lineageos/updater/UpdatesActivity.java
@@ -487,18 +487,18 @@ public class UpdatesActivity extends UpdatesListActivity {
         Spinner autoCheckInterval = view.findViewById(R.id.preferences_auto_updates_check_interval);
         SwitchCompat autoDelete = view.findViewById(R.id.preferences_auto_delete_updates);
         SwitchCompat dataWarning = view.findViewById(R.id.preferences_mobile_data_warning);
-        SwitchCompat abPerfMode = view.findViewById(R.id.preferences_ab_perf_mode);
+        //SwitchCompat abPerfMode = view.findViewById(R.id.preferences_ab_perf_mode);
         SwitchCompat updateRecovery = view.findViewById(R.id.preferences_update_recovery);
 
-        if (!Utils.isABDevice()) {
+        /*if (!Utils.isABDevice()) {
             abPerfMode.setVisibility(View.GONE);
-        }
+        }*/
 
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         autoCheckInterval.setSelection(Utils.getUpdateCheckSetting(this));
         autoDelete.setChecked(prefs.getBoolean(Constants.PREF_AUTO_DELETE_UPDATES, false));
         dataWarning.setChecked(prefs.getBoolean(Constants.PREF_MOBILE_DATA_WARNING, true));
-        abPerfMode.setChecked(prefs.getBoolean(Constants.PREF_AB_PERF_MODE, false));
+        //abPerfMode.setChecked(prefs.getBoolean(Constants.PREF_AB_PERF_MODE, false));
 
         if (getResources().getBoolean(R.bool.config_hideRecoveryUpdate)) {
             // Hide the update feature if explicitly requested.
@@ -537,7 +537,7 @@ public class UpdatesActivity extends UpdatesListActivity {
                                     autoCheckInterval.getSelectedItemPosition())
                             .putBoolean(Constants.PREF_AUTO_DELETE_UPDATES, autoDelete.isChecked())
                             .putBoolean(Constants.PREF_MOBILE_DATA_WARNING, dataWarning.isChecked())
-                            .putBoolean(Constants.PREF_AB_PERF_MODE, abPerfMode.isChecked())
+                            //.putBoolean(Constants.PREF_AB_PERF_MODE, abPerfMode.isChecked())
                             .apply();
 
                     if (Utils.isUpdateCheckEnabled(this)) {
